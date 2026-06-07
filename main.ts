365f3baf2dabb9ae78e1eb71a2d1eb259874ba02
muncherelli <16467105+muncherelli@users.noreply.github.com>
2026-06-07 13:32:12 -0500

    chore: remove debug logging functionality from turbopanel-daemon, simplifying main execution flow and enhancing overall code clarity

--- main.ts
+++ main.ts
@@ -1,36 +1,10 @@
 import { connectInstance } from './src/instance/client.ts'
 import { initOrchestration } from './src/orchestration/setup.ts'
 import { startTunnels } from './src/tunnels.ts'
-import {
-  agentDebugLog,
-  probeLogDirectory,
-  readParentComm,
-} from './src/debug-log.ts'
 
 console.log('Hello from turbopanel-daemon')
 
-// #region agent log
-const logDir = '/var/log/turbopanel/daemon'
-const parentComm = await readParentComm()
-const logProbe = await probeLogDirectory(logDir)
-await agentDebugLog('main.ts:startup', 'daemon startup logging probe', {
-  hostname: Deno.hostname(),
-  pid: Deno.pid,
-  parentComm,
-  systemdUnitEnv: Deno.env.get('INVOCATION_ID') ?? null,
-  logProbe,
-}, 'H1')
-// #endregion
-
 await initOrchestration()
-
-// #region agent log
-const logProbeAfter = await probeLogDirectory(logDir)
-await agentDebugLog('main.ts:post-orchestration', 'post-orchestration logging probe', {
-  parentComm: await readParentComm(),
-  logProbe: logProbeAfter,
-}, 'H3')
-// #endregion
 
 const abort = new AbortController()
 
